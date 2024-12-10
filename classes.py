468c0ff2ea6fdd43366972c12b42c08217eb4621
nf317881 <nf317881@gmail.com>
2024-12-10 16:13:48 -0500

    Made conditional class, start processing courses

--- classes.py
+++ classes.py
@@ -1,25 +1,39 @@
+
 class Course:
-    def __init__(self, course_number, name, semester=None, credits=0, prerequisites=None, corequisites=None, restrictions=None, desc=""):
-        if prerequisites is None:
-            prerequisites = []
+    def __init__(self, id, name, semester, credits, reenrollment_info=None, prerequisites=None, corequisites=None, recommended_background=None,
+                 restrictions=None, not_open_with_credit=None, description=None, alias=None, interdepartmental=None,
+                 administered_by=None, effective_dates=None):
+
+        if not_open_with_credit is None:
+            not_open_with_credit = []
         if corequisites is None:
             corequisites = []
-        if semester is None:
-            semester = []
+        if prerequisites is None:
+            prerequisites = []
 
-        self.prerequisites = prerequisites
-        self.course_number = course_number
+        self.id = id
         self.name = name
         self.semester = semester
         self.credits = credits
+        self.reenrollment_info = reenrollment_info
+        self.prerequisites = prerequisites
+        self.corequisites = corequisites
+        self.recommended_background = recommended_background
         self.restrictions = restrictions
-        self.desc = desc
+        self.not_open_with_credit = not_open_with_credit
+        self.description = description
+        self.alias = alias
+        self.interdepartmental = interdepartmental
+        self.administered_by = administered_by
+        self.effective_dates = effective_dates
 
     def __str__(self):
         return self.name
 
     def __repr__(self):
         return self.__str__()
+
+
 
 class Major:
     def __init__(self, name, desc, courses=None):
@@ -34,3 +48,79 @@ class Major:
 
     def __repr__(self):
         return self.__str__()
+
+
+
+class Conditional:
+    def __init__(self, str_cond):
+        opening_parenthesis = 0
+        for char in str_cond:
+            if char == '(':
+                opening_parenthesis += 1
+            elif char == ')':
+                opening_parenthesis -= 1
+
+            index = str_cond.index(char)
+            if char == 'a' and str_cond[index + 1] == 'n' and str_cond[index + 2] == 'd' and \
+                opening_parenthesis == 0:
+
+                self.split_type = "and"
+                self.split = index
+
+            if char == 'o' and str_cond[index + 1] == 'r' and opening_parenthesis == 0:
+                self.split_type = "or"
+                self.split = []
+                self.split.append(index)
+
+
+        self.conds = []
+        if self.split_type == "and":
+            if str_cond[self.split+4:] != "Concurrently":
+                cond_1 = str_cond[:self.split-1]
+                cond_2 = str_cond[self.split+4:]
+
+                if "or" in cond_1 or "and" in cond_1:
+                    if "(" in cond_1:
+                        cond_1 = Conditional(cond_1[1:-1])
+                    else:
+                        cond_1 = Conditional(cond_1)
+
+                if "or" in cond_2 or "and" in cond_2:
+                    if "(" in cond_2:
+                        cond_2 = Conditional(cond_2[1:-1])
+                    else:
+                        cond_2 = Conditional(cond_2)
+
+                self.conds.append(cond_1)
+                self.conds.append(cond_2)
+            else:
+                self.split_type = "concurrent"
+                self.conds = str_cond[:self.split-1]
+
+        elif self.split_type == "or":
+            for x in range(len(self.split)):
+                if x == 0:
+                    self.conds.append(str_cond[:self.split[x]-1])
+                elif x == len(self.split)-1:
+                    self.conds.append(str_cond[self.split[x]+3:])
+                else:
+                    self.conds.append(str_cond[self.split[x-1]+3:self.split[x]-1])
+
+        else:
+            self.split_type = "none"
+            self.conds = str_cond
+
+    def check_if_courses_in(self, course_id_list_previous, course_id_list_current):
+        if self.split_type == "none":
+            return_value = self.conds in course_id_list_previous
+        elif self.split_type == "and":
+            return_value = self.conds[0].check_if_courses_in(course_id_list_previous) and self.conds[1].check_if_courses_in(course_id_list_previous)
+        elif self.split_type == "or":
+            return_value = False
+            for x in self.conds:
+                if x.check_if_courses_in(course_id_list_previous):
+                    return_value = True
+        elif self.split_type == "concurrent":
+            return_value = self.conds in course_id_list_current or self.conds in course_id_list_previous
+
+        return return_value
--- pullCourses.py
+++ pullCourses.py
@@ -4,7 +4,7 @@ import selenium.webdriver.chrome
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.select import Select
 
-import classes
+from classes import *
 
 def get_html():
     url = "https://reg.msu.edu/Courses/search.aspx"
@@ -13,7 +13,7 @@ def get_html():
     subject = driver.find_element(By.NAME, "ctl00$MainContent$ddlSubjectCode")
 
     subject = Select(subject)
-    subject.select_by_index(1)
+    subject.select_by_index(2)
 
     search_button = driver.find_element(By.NAME, "ctl00$MainContent$btnSubmit")
     search_button.click()
@@ -38,20 +38,93 @@ def get_courses(html_source):
     for course in courses_list:
         id, name = course[0].split("  ")
 
-        semester = course[2]
-        if ", " in semester:
-            semester = semester.split(", ")
-        else:
-            semester = [semester]
-        for x in range(len(semester)):
-            semester[x] = semester[x].split(" ")[0]
+        semester = dict()
+        credits = []
+        reenroll_info = None
+        prereqs = None
+        coreqs = None
+        recommended_background = None
+        restrictions = None
+        not_open_with_credit = None
+        description = None
+        semester_alias = None
+        interdepartmental_with = None
+        administered_by = None
+        effective_dates = None
 
-        credits = course[4]
-        credits = credits.split("  ")[0]
-        credits = credits.split(" ")[-1]
-        credits = int(credits)
+        extras = course[1:]
+        for x in range(int(len(extras)/2)):
+            descriptor = extras[x*2]
+            data = extras[x*2+1]
 
+            if descriptor == "Semester:":
+                if ", " in data:
+                    semester = data.split(", ")
+                else:
+                    semester = [data]
+                for y in range(len(semester)):
+                    semester[y] = semester[y].split(" ")[0]
 
+            elif descriptor == "Credits:":
+                credits = data
+                credits = credits.split("  ")
+                credits_dict = dict()
+                for x in credits:
+                    if "Total" in x:
+                        credits_dict["Total"] = int(x.split(" ")[-1])
+                    elif "Lecture" in x:
+                        credits_dict["Lecture"] = int(x.split(" ")[-1])
+                    elif "Lab" in x:
+                        credits_dict["Lab"] = int(x.split(" ")[-1])
+                if len(credits_dict) > 0:
+                    credits = credits_dict
+                else:
+                    credits = data
+
+            elif descriptor == "Reenrollment Information:":
+                reenroll_info = data
+
+            elif descriptor == "Prerequisite:":
+                test = 0
+
+            elif descriptor == "Corequisite:":
+                coreqs = data
+                coreqs = coreqs.split(" ")
+                coreqs = " ".join(coreqs[:2])
+
+            elif descriptor == "Recommended Background:":
+                recommended_background = data
+
+            elif descriptor == "Restrictions:":
+                restrictions = data
+
+            elif descriptor == "Not open to students with credit in:":
+                not_open_with_credit = data
+
+            elif descriptor == "Description:":
+                description = data
+
+            elif descriptor == "Semester Alias:":
+                semester_alias = data
+
+            elif descriptor == "Interdepartmental With:":
+                interdepartmental_with = data
+
+            elif descriptor == "Administered By:":
+                administered_by = data
+
+            elif descriptor == "Effective Dates:":
+                effective_dates = data
+
+        index = courses_list.index(course)
+        courses_list[index] = Course(id=id, name=name, semester=semester, credits=credits,
+                                     reenrollment_info=reenroll_info, prerequisites=prereqs, corequisites=coreqs,
+                                     recommended_background=recommended_background, restrictions=restrictions,
+                                     not_open_with_credit=not_open_with_credit, description=description,
+                                     alias=semester_alias, interdepartmental=interdepartmental_with,
+                                     administered_by=administered_by, effective_dates=effective_dates)
+
+    return courses_list
 
 
 
